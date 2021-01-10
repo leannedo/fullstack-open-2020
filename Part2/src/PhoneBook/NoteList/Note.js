b95373133514a7ad17827a7691dd86297a8223a9
ngocdo <ngocdo0912@gmail.com>
2021-01-10 11:36:11 +0200

    Complete exercise phonebook and country

---
+++ Part2/src/PhoneBook/NoteList/Note.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Note = ({ note }) => {
+  return (
+    <li>
+      <h4>{note}</h4>
+    </li>
+  );
+};
+
+export default Note;
--- Part2/src/config/routes.js
+++ Part2/src/config/routes.js
@@ -1,10 +1,11 @@
 // paths
-import { COURSE_INFO_PATH, HOME_PATH, PHONE_BOOK } from "./paths";
+import { COURSE_INFO_PATH, HOME_PATH, PHONE_BOOK, COUNTRY } from "./paths";
 
 // views
 import Home from "../Home";
 import CourseInfo from "../Courseinfo/CourseInfo";
 import PhoneBook from "../PhoneBook/PhoneBook";
+import Country from "../Country/Country";
 
 export const routes = [
   {
@@ -18,5 +19,9 @@ export const routes = [
   {
     path: PHONE_BOOK,
     component: PhoneBook,
+  },
+  {
+    path: COUNTRY,
+    component: Country,
   },
 ];
